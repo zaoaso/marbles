92e325954baab73d212841507fcf9f0aad07629d
zaoaso <jungman@kr.ibm.com>
2017-02-03 17:22:02 +0900

    Update app.js

--- app.js
+++ app.js
@@ -28,8 +28,8 @@ var fs = require('fs');
 var cors = require('cors');
 
 //// Set Server Parameters ////
-var host = setup.SERVER.HOST;
-var port = setup.SERVER.PORT;
+var host = prcess.env.PORT;
+var port = process.env.VCAP_AOO_HOST||"0.0.0.0";
 
 ////////  Pathing and Module Setup  ////////
 app.set('views', path.join(__dirname, 'views'));
